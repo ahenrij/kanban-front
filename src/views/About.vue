614d160aefd5779bcbdd8d466eb2cb179a09b0d2
Henri Aïdasso <ahenrij@gmail.com>
2022-03-19 12:11:53 +0100

    about updated

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="about">
-    <span class="uk-text-lead uk-text-notmal">KanbanApp</span><br>
-    <span class="uk-text-light" style="font-size: 1.2em;">version 1.0.0</span>
-    <p>Plateforme de gestion et de suivi de tâches kanban. <br> © Université de Rennes 1 - M1 MIAGE - 2021</p>
+  <div class="about uk-flex uk-flex-middle uk-flex-center">
+    <div>
+      <span class="uk-text-lead uk-text-notmal">Kanban Board</span><br>
+      <span class="uk-text-light" style="font-size: 1.2em;">version 1.0.0</span>
+      <p>Plateforme de gestion et de suivi de tâches kanban. <br> © 2022 . Henri Aïdasso</p>
+    </div>
   </div>
 </template>
 
@@ -11,7 +13,7 @@
     font-family: 'Monsterrat';
   }
   .about {
-    padding: 200px 25px 20px 25px;
+    padding: 250px 25px 20px 25px;
   }
   .uk-text-lead {
     font-size: 3em;
